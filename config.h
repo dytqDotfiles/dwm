60aff7c69a94b5781451a65f1aaf75409f79ab68
dytq <taariq.deda@gmail.com>
2021-01-17 14:49:07 +0100

    add my config

--- config.h
+++ config.h
@@ -1,21 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=20","fontawesome:size:20","JoyPixels:pixelsize=20:antialias=true:autohint=true" };
+static const char dmenufont[]       = "monospace:size=20";
+/* default color */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+/*retro color scheme */
+static const char col_backW[]       = "#202732";
+static const char col_bordunfocW[]  = "#526667";
+static const char col_bordfocusW[]  = "#8F6A61";
+static const char col_bar[]         = "#8CD2EE";
+static const char col_txt[]         = "#FEFEFE";
+static const char col_bartxt[]      = "#FEFEFE";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg          bg         border   */
+	[SchemeNorm] = { col_bartxt, col_backW, col_bordunfocW },
+	[SchemeSel]  = { col_txt,    col_bar,   col_bordfocusW },
 };
 
 /* tagging */
@@ -38,13 +49,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[T]",      tile },    /* first entry is default */
+	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -57,19 +68,50 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "xterm", NULL };
+
+/* microphone */
+static const char *mic_mut[] = { "amixer", "set", "Capture", "toggle", NULL};
+static const char *mic_register[] = {NULL}; //to do
+/* lock screen */
+static const char *lockscr[] = { "slock",NULL};
+
+/* Special Keys define XF86 lib */
+#define XK_AudioM XF86XK_AudioMute 
+#define XK_AudioL XF86XK_AudioLowerVolume 
+#define XK_AudioR XF86XK_AudioRaiseVolume	
+#define XK_BriUp  XF86XK_MonBrightnessUp 
+#define XK_BriDo  XF86XK_MonBrightnessDown
+#define XK_Mic    XF86XK_AudioMicMute
+
+/* install brightnessctl */ 
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ 0,				XK_AudioM, spawn,          SHCMD("sh ~/.scripts/volume_mute.sh")},
+       	{ 0, 				XK_AudioL, spawn, 	   SHCMD("sh ~/.scripts/volume_down.sh")},
+	{ 0,				XK_AudioR, spawn, 	   SHCMD("sh ~/.scripts/volume_up.sh")},	
+	{ MODKEY,			XK_AudioM, spawn,          SHCMD("mpc prev")},
+       	{ MODKEY,			XK_AudioL, spawn, 	   SHCMD("mpc toggle")},
+	{ MODKEY,			XK_AudioR, spawn, 	   SHCMD("mpc next")},
+	{ MODKEY|ShiftMask,		XK_AudioL, spawn,	   SHCMD("mpc stop")},	
+	{ 0,	 			XK_BriUp,  spawn, 	   SHCMD("brightnessctl --quiet set +10%")},
+	{ 0, 				XK_BriDo,  spawn, 	   SHCMD("brightnessctl --quiet set 10%-")},
+	{ 0, 				XK_Print,  spawn,	   SHCMD("scrot -d0 ~/Image/%Y-%m-%d-%s_$wx$h.jpg") }, 
+	{ 0,				XK_Mic,    spawn,	   {.v = mic_mut } },
+	{ MODKEY,			XK_Mic,    spawn,	   {.v = mic_register } }, 
+	{ Mod4Mask,			XK_l,      spawn,	   {.v = lockscr } },
+	{ MODKEY|ShiftMask,		XK_s,	   spawn,	   SHCMD("sh ~/.scripts/dmenu/shutdown.sh") },
+	{ MODKEY|ShiftMask,		XK_r,	   spawn, 	   SHCMD("sh ~/.scripts/dmenu/reboot.sh") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	//{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_k,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -84,18 +126,18 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_ampersand,                     0)
+	TAGKEYS(                        XK_eacute,                        1)
+	TAGKEYS(                        XK_quotedbl,                      2)
+	TAGKEYS(                        XK_apostrophe,                    3)
+	TAGKEYS(                        XK_parenleft,                     4)
+	TAGKEYS(                        XK_minus,                         5)
+	TAGKEYS(                        XK_egrave,                        6)
+	TAGKEYS(                        XK_underscore,                    7)
+	TAGKEYS(                        XK_ccedilla,                      8)
+	
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
-
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
